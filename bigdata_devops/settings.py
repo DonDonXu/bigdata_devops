8797e7010e8b21b766b3ae8099e02f853c175d3c
zfno111 <zfno111@163.com>
2019-07-18 09:13:59 +0800

    setting  允许所有主机访问

--- bigdata_devops/settings.py
+++ bigdata_devops/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f%9v_r_bop=2yh%v4s@q2zqwt+-$u_iaspcvrjm0^hqhk5w@^7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
